8ce693b0a2cbdd3af67019149eb2b24ae32240b2
AntonBiryukovUofC <anton.biryukov@ucalgary.ca>
2016-05-25 13:16:11 -0600

    commands_run_thecodes

--- run_WI_with_velmodel.py
+++ run_WI_with_velmodel.py
@@ -1,6 +1,7 @@
 import shutil
 import numpy as np
 import os
+import commands
 import glob
 import matplotlib.pyplot as plt
 
@@ -26,19 +27,26 @@ plt.axis('equal')
 for i in range(len(stationAzimuths)):
     plt.annotate(str(i), (stationCoords[i,0],stationCoords[i,1]))
 i=-1
+station_names.pop()
 for station_dest in station_names:
     #continue
+    status, output = commands.getstatusoutput("rm *.{E,N,Z}0")
+    status, output = commands.getstatusoutput("rm hspec96.{dat,grn,out}")
+    status, output = commands.getstatusoutput("rm g1.vel")
+
     i+=1
     dfile = station_dest+"sta_dfile"
     FHS = station_dest + "eq_depth"
     FHR = station_dest + "sta_depth"
     command_to_hprep96 = "hprep96 -M %s -d %s -FHS %s -FHR %s -EQEX " % tuple ((vel_filename,dfile,FHS,FHR))  
-    command_to_hspec96 = "hspec96 > hspec96.out"
-    command_to_hpulse96 = "hpulse96 -V -p -l 1 > g1.vel"
+    #command_to_hprep96 = "hprep96 -TH -M %s -d %s -FHS %s -FHR %s -EQEX " % tuple ((vel_filename,dfile,FHS,FHR))  
+    command_to_hspec96 = "hspec96 | tee hspec96.out"
+    #command_to_hpulse96 = "hpulse96 -V -p -l 1 > g1.vel"
+    command_to_hpulse96 = "hpulse96 -D -p -l 1 > g1.vel"
     #tensor_xx_yy_zz_xy_xz_yz = [1,1,1,0,0,0]
     tensor = [1,1,1,0,0,0]
     line_to_fmech96 = "fmech96 -XX %3.3f " + "-YY %3.3f "+ "-ZZ %3.3f " + "-XY %3.3f "+ \
-                "-XZ %3.3f "+ "-YZ %3.3f " + "-A %3.3f " + "-ROT < g1.vel | f96tosac -G" 
+                "-XZ %3.3f "+ "-YZ %3.3f " + "-A %3.3f " + " < g1.vel | f96tosac -B" 
     if np.isnan(stationAzimuths[i]):
         stationAzimuths[i] = 0
     command_to_fmech96 = line_to_fmech96 % tuple((tensor[0],tensor[1],tensor[2],tensor[3],tensor[4],
@@ -47,10 +55,19 @@ for station_dest in station_names:
     #rm hspec96*
     #rm dfile    
     
-    print command_to_hprep96
-    #os.system(command_to_hprep96)
-
-
+    
+    status, output = commands.getstatusoutput(command_to_hprep96)
+    print output
+    
+    status, output = commands.getstatusoutput(command_to_hspec96)
+    print output
+    
+    status, output = commands.getstatusoutput(command_to_hpulse96)
+    print output
+    
+    status, output = commands.getstatusoutput(command_to_fmech96)
+    print output
+    
 
 
 
