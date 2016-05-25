092cc81772b95839a662d75a72b160763811afcb
anton.biryukov@ucalgary.ca <anton.biryukov@ucalgary.ca>
2016-05-25 00:47:25 -0400

    commands os done

--- run_WI_with_velmodel.py
+++ run_WI_with_velmodel.py
@@ -25,17 +25,24 @@ plt.plot(stationCoords[-1,0],stationCoords[-1,1],'go')
 plt.axis('equal')
 for i in range(len(stationAzimuths)):
     plt.annotate(str(i), (stationCoords[i,0],stationCoords[i,1]))
-
+i=-1
 for station_dest in station_names:
     #continue
+    i+=1
     dfile = station_dest+"sta_dfile"
     FHS = station_dest + "eq_depth"
     FHR = station_dest + "sta_depth"
     command_to_hprep96 = "hprep96 -M %s -d %s -FHS %s -FHR %s -EQEX " % tuple ((vel_filename,dfile,FHS,FHR))  
     command_to_hspec96 = "hspec96 > hspec96.out"
     command_to_hpulse96 = "hpulse96 -V -p -l 1 > g1.vel"
-    tensor_xx_yy_zz_xy_xz_yz = [1,1,1,0,0,0]
-    command_to_fmech96 = "fmech96 -XX %3.3f -YY %3.3f -ZZ %3.3f -XY %3.3f -XZ %3.3f -YZ %3.3f -A %3.3f -ROT < g1.vel | f96tosac -G" # not done
+    #tensor_xx_yy_zz_xy_xz_yz = [1,1,1,0,0,0]
+    tensor = [1,1,1,0,0,0]
+    line_to_fmech96 = "fmech96 -XX %3.3f " + "-YY %3.3f "+ "-ZZ %3.3f " + "-XY %3.3f "+ \
+                "-XZ %3.3f "+ "-YZ %3.3f " + "-A %3.3f " + "-ROT < g1.vel | f96tosac -G" 
+    if np.isnan(stationAzimuths[i]):
+        stationAzimuths[i] = 0
+    command_to_fmech96 = line_to_fmech96 % tuple((tensor[0],tensor[1],tensor[2],tensor[3],tensor[4],
+                                                  tensor[5],stationAzimuths[i]))
     #rm g1.vel
     #rm hspec96*
     #rm dfile    
