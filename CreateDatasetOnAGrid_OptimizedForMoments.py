d00468240200a0596c2d3238ada112a80d9db44d
AntonBiryukovUofC <anton.biryukov@ucalgary.ca>
2016-06-17 17:05:06 -0600

    optimizing modelling

--- CreateDatasetOnAGrid_OptimizedForMoments.py
+++ CreateDatasetOnAGrid_OptimizedForMoments.py
@@ -1,9 +1,9 @@
 import numpy as np
 from LocationsOnGrid import LocationsOnGrid
-from runWIwithVelmodel_func import RunWIModel
+from runWIwithVelmodel_func import RunWIModelNoTensor
 import subprocess
 
-
+# 3166 4814 3910 - receiver at
 # Get the location grid for the potential earthquake origins:
 
 Ntensors = 8
@@ -27,20 +27,22 @@ tensor = np.array([0,0,0,1,0,1])
 sigma = 0.5/2
 for i in range(np.shape(xv)[0]):
     for j in range(np.shape(xv)[1]):
+        location_no_tensor = "./"+"Row"+str(i)+"Col"+str(j) + "/"
+        RunWIModelNoTensor(prefix_dest= './temp/', velname='VpVs.dat', fname = 'receiver.dat',sname = sub_source_dir + "/" + "source"+"_"+str(i)+"_"+str(j),
+                           tMax = 3.4)
+        
         for k in range(Ntensors):
             tPerturb = np.random.normal(0,sigma,6)
             train_tensor = tensor + tPerturb
             #print train_tensor
             
             tensor_id_str = str(k)
-            location_with_tensor = "./"+"Row"+str(i)+"Col"+str(j) + "/" +"Moment"+ tensor_id_str +"/"
+            
             print location_with_tensor
             
             
     
 
-            RunWIModel(prefix_dest= location_with_tensor, velname='VpVs.dat', fname = 'receiver.dat',sname = sub_source_dir + "/" + "source"+"_"+str(i)+"_"+str(j),
-                           tMax = 3.4,tensor = train_tensor)
             
         
         #RunWIModel(prefix_dest= location_with_tensor, velname='VpVs.dat', fname = 'receiver.dat',sname = 'source.dat', tMax = 3.4,tensor = train_tensor)
--- runWIwithVelmodel_func.py
+++ runWIwithVelmodel_func.py
@@ -25,10 +25,11 @@ def RunWIModel(prefix_dest= '', velname='VpVs.dat', fname = 'receiver.dat',sname
         plt.annotate(str(stationAzimuths[i]), (stationCoords[i,0]+100,stationCoords[i,1]+100))
         plt.savefig(prefix_dest + "receiver-source.png")
     i=-1
-    station_names.pop()
+    #station_names.pop()
 
 #tensor = [6.15, 10, 19.5, -34.3, -3.61, 34.1]
 #returnnn
+    print station_names
     for station_dest in station_names:
     # Prefix - for the folder where folders with station data are located
         station_dest = station_dest
@@ -64,6 +65,71 @@ def RunWIModel(prefix_dest= '', velname='VpVs.dat', fname = 'receiver.dat',sname
         command_to_copy = "cp -v *.sac " + station_dest
         status, output = commands.getstatusoutput(command_to_copy)   
         print output
+
+
+
+def RunWIModelNoTensor(prefix_dest= '', velname='VpVs.dat', fname = 'receiver.dat',sname = 'source.dat', tMax = 3.4):
+# Temporary: delete the station folders
+#if len(glob.glob("station*"))>0:
+#   map(shutil.rmtree,glob.glob("station*"))
+
+
+# Build a velocity model file
+    vel_filename = makeVelocityModel(velname)
+# Build folders with stations
+    station_names, stationCoords, stationAzimuths,sourceCoords = MakeStationAndSourceFiles(fname,sname,tMax,prefix_dest)
+    stationCoords = np.matrix(stationCoords)
+
+    plt.close('all')
+    plt.plot(stationCoords[:,0],stationCoords[:,1],'ro')
+    plt.plot(sourceCoords[0],sourceCoords[1],'go')
+    plt.axis('equal')
+    for i in range(len(stationAzimuths)):
+        plt.annotate(str(i+1), (stationCoords[i,0],stationCoords[i,1]))
+        plt.annotate(str(stationAzimuths[i]), (stationCoords[i,0]+100,stationCoords[i,1]+100))
+        plt.savefig(prefix_dest + "receiver-source.png")
+    i=-1
+    #station_names.pop()
+
+#tensor = [6.15, 10, 19.5, -34.3, -3.61, 34.1]
+#returnnn
+    for station_dest in station_names:
+    # Prefix - for the folder where folders with station data are located
+        station_dest = station_dest
+        i+=1
+        dfile = station_dest+"sta_dfile"
+        FHS = station_dest + "eq_depth"
+        FHR = station_dest + "sta_depth"
+        outputStation = station_dest  + "gStation"+str(i+1)+".vel"
+        command_to_hprep96 = "hprep96 -M %s -d %s -FHS %s -FHR %s -BH -ALL" % tuple ((vel_filename,dfile,FHS,FHR))  
+        command_to_hspec96 = "hspec96 | tee hspec96.out"
+        command_to_hpulse96 = "hpulse96 -D -p -l 1 > " + outputStation
+       # line_to_fmech96 = "fmech96 -XX %3.3f " + "-YY %3.3f "+ "-ZZ %3.3f " + "-XY %3.3f "+ "-XZ %3.3f "+ "-YZ %3.3f " + "-A %3.3f " + "-B %3.3f"  + "  < g1.vel | f96tosac -B" 
+        if np.isnan(stationAzimuths[i]):
+            stationAzimuths[i] = 0
+        # command_to_fmech96 = line_to_fmech96 % tuple((tensor[0],tensor[1],tensor[2],tensor[3],tensor[4],tensor[5],stationAzimuths[i],stationAzimuths[i]+180))
+        print station_dest
+    
+
+        status, output = commands.getstatusoutput(command_to_hprep96)
+        print command_to_hprep96
+        print output
+    
+        status, output = commands.getstatusoutput(command_to_hspec96)
+        print command_to_hspec96
+        #print output
+    
+        status, output = commands.getstatusoutput(command_to_hpulse96)
+        print command_to_hpulse96
+        print output
+    
+        #status, output = commands.getstatusoutput(command_to_fmech96)
+        #print command_to_fmech96
+        #print output
+        #command_to_copy = "cp -v *.sac " + station_dest
+        #status, output = commands.getstatusoutput(command_to_copy)   
+        #print output
+
 
 
 
